a169913200f83aec2cbd0a41e45cc3580c18a3c1
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-04-01 23:14:51 +0300

    add tekstura.jpg

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import {App} from './n1-main/m1-ui/App';
 import {Provider} from 'react-redux';
 import store from './n1-main/m2-bll/store';
 
-//Hy!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
 ReactDOM.render(
     <React.StrictMode>
