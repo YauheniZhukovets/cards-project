447a4fbe9b4f3212f9b0941beadb1390b0da4e72
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-04-03 21:21:49 +0300

    create style Forgot page

--- src/n2-features/f1-auth/a3-password/NewPassword.tsx
+++ src/n2-features/f1-auth/a3-password/NewPassword.tsx
@@ -1,19 +1,46 @@
 import React from 'react';
-import style from "../a2-registration/Login.module.css";
+import style from '../../../n1-main/m1-ui/styles/Login.module.css';
 
 export const NewPassword = () => {
     return (
-        <div>
+        <div className={style.mainContainer}>
             <div className={style.container_log} >
-                <h2 className={style.title} style={{color:'black'}}>IT-incubator</h2>
-                <h2 className={style.subtitle} style={{color:'black'}}> Forgot your password?</h2>
+                <div className={style.title} >
+                    <h1>Forgot</h1>
+                </div>
+                <div className={style.subtitle}>
+                    <h2> Forgot your password?</h2>
+                </div>
+                <div>
+                    <form >
+                        <label >Email</label>
+                        <input type="email"/>
+                        <h4 style={{color:'black', marginTop:'0px'}}>Enter your email address and we will send you further instructions</h4>
+                        <div className={style.btnContainer} style={{marginTop:'95px'}} >
 
-                <input type="rmail"/><span style={{color:'black'}}>Enter your email address and we will send you further instructions</span>
-                <button>Send Instructions</button>
-                <p>Did you remember your password?</p>
-                <a>Try logging in</a>
+                            <button className={style.btn} style={{width:'200px'}}>Send Instructions</button>
+                        </div>
+                    </form>
+                </div>
+                <div className={style.hContainer}>
+                    <h4 style={{color:'black'}}>Did you remember your password?</h4>
+                </div>
+                <div className={style.aContainer}>
+                    <a href='#' className={style.linkUp}>Try logging in</a>
+                </div>
             </div>
         </div>
+        // <div>
+        //     <div className={style.container_log} >
+        //         <h2 className={style.title} style={{color:'black'}}>IT-incubator</h2>
+        //         <h2 className={style.subtitle} style={{color:'black'}}> Forgot your password?</h2>
+        //
+        //         <input type="rmail"/><span style={{color:'black'}}>Enter your email address and we will send you further instructions</span>
+        //         <button>Send Instructions</button>
+        //         <p>Did you remember your password?</p>
+        //         <a>Try logging in</a>
+        //     </div>
+        // </div>
     );
 };
 
--- src/n2-features/f1-auth/a3-password/PasswordRecovery.tsx
+++ src/n2-features/f1-auth/a3-password/PasswordRecovery.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
-import style from "../a2-registration/Login.module.css";
+import style from '../../../n1-main/m1-ui/styles/Login.module.css';
 
 export const PasswordRecovery = () => {
     return (
-        <div>
+        <div className={style.mainContainer}>
             <div className={style.container_log} >
-                <h2 className={style.title} style={{color:'black'}}>IT-incubator</h2>
-                <h2 className={style.subtitle} style={{color:'black'}}> Create new password</h2>
-
-                <input type="rmail"/><span style={{color:'black'}}>Create new password and we will send you further instructions to email</span>
-                <button>Create new password</button>
-
+                <div className={style.title} >
+                    <h1>Register</h1>
+                </div>
+                <div className={style.subtitle}>
+                    <h2> Sing Up</h2>
+                </div>
+                <div>
+                    <form >
+                        <label >Email</label>
+                        <input type="email"/>
+                        <label >Password</label>
+                        <a href='#'><input className={style.eyes} type="password"/></a>
+                        <label >Confirm password</label>
+                        <a href='#'><input className={style.eyes} type="password"/></a>
+                    </form>
+                </div>
+                <div className={style.btnContainer}>
+                    <button className={style.btnDise}>Cancel</button>
+                    <button className={style.btn}>Register</button>
+                </div>
             </div>
         </div>
+
+        // <div>
+        //     <div className={style.container_log} >
+        //         <h2 className={style.title} style={{color:'black'}}>IT-incubator</h2>
+        //         <h2 className={style.subtitle} style={{color:'black'}}> Create new password</h2>
+        //
+        //         <input type="rmail"/><span style={{color:'black'}}>Create new password and we will send you further instructions to email</span>
+        //         <button>Create new password</button>
+        //
+        //     </div>
+        // </div>
     );
 };
