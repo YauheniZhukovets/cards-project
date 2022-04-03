135ab04ce291deb640267fc42f164c45293bd500
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-04-03 22:35:09 +0300

    completed the functionality of the SignIn page

--- src/n1-main/m1-ui/heder/Header.tsx
+++ src/n1-main/m1-ui/heder/Header.tsx
@@ -3,15 +3,14 @@ import {NavLink} from 'react-router-dom';
 import s from './Header.module.css'
 import {PATH} from '../routes/RoutesRoot';
 
-
 const setActive = ({isActive}: { isActive: boolean }) => isActive ? s.activeLink : s.item
 
 export const Header = () => {
 
     return (
         <div>
-            <NavLink className={setActive} to={PATH.LOGIN}> <b> Login </b> </NavLink>
             <NavLink className={setActive} to={PATH.REGISTRATION}> <b> Registration</b> </NavLink>
+            <NavLink className={setActive} to={PATH.LOGIN}> <b> Login </b> </NavLink>
         </div>
     );
 }
