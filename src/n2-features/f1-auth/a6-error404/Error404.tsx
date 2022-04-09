45c07e228328bc732b11677ced7d98573d2e1fd5
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-04-09 22:05:37 +0300

    creat error page

--- src/n2-features/f1-auth/a6-error404/Error404.tsx
+++ src/n2-features/f1-auth/a6-error404/Error404.tsx
@@ -3,21 +3,19 @@ import {NavLink} from 'react-router-dom';
 import s from './Error404.module.css'
 import SuperButton from '../../../n1-main/m1-ui/common/c2-SuperButton/SuperButton';
 import {PATH} from '../../../n1-main/m1-ui/routes/RoutesRoot';
-import style from '../../../n1-main/m1-ui/styles/EditProfile.module.css';
-import img404 from './404.png'
+import img404 from './error.png'
 
 export function Error404() {
     return (
         <div className={s.errorBlock}>
-            <img src={img404} alt="404"/>
-            <div>Not Found</div>
-            <div>
-                <NavLink to={PATH.PROFILE}><SuperButton
-                    className={style.btn}
-                    style={{background: 'rgba(232, 226, 226, 0.6)', color: '#2D2E46'}}>Go to homepage
+            <div className={s.title}>Not Found</div>
+            <div className={s.btnBox}>
+                <NavLink to={PATH.PROFILE}><SuperButton className={s.btn}>
+                    Go to homepage
                 </SuperButton>
                 </NavLink>
             </div>
+            <img src={img404} alt="404"/>
 
         </div>
     )
