cf9be791d605ec690e2af29142a3d34afcfbd82a
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-04-11 23:56:30 +0300

    add Thunk creator(addPack, deletePack, updatePack)

--- src/n2-features/f2-cards/Packs/PackTable/Pack.tsx
+++ src/n2-features/f2-cards/Packs/PackTable/Pack.tsx
@@ -28,16 +28,18 @@ export const Pack: React.FC<PackPropsType> = ({pack}) => {
     }
 
     return (
-            <div className={s.item}>
-                <div className={s.nameItem}>{pack.name}</div>
-                <div className={s.cardsCount}>{pack.cardsCount}</div>
-                <div className={s.updated}>{pack.updated.slice(0, 10)}</div>
-                <div className={s.buttons}>{myUserId === pack.user_id && <>
+        <div className={s.item}>
+            <div className={s.nameItem}>{pack.name}</div>
+            <div className={s.cardsCount}>{pack.cardsCount}</div>
+            <div className={s.updated}>{pack.updated.slice(0, 10)}</div>
+            <div className={s.buttons}>{myUserId === pack.user_id &&
+                <>
                     <button onClick={onClickDeletePackHandler}>Delete</button>
                     <button onClick={onClickUpdatePackHandler}>Edit</button>
                     <button>Learn</button>
-                </>}
-                </div>
+                </>
+            }
             </div>
+        </div>
     );
 };
