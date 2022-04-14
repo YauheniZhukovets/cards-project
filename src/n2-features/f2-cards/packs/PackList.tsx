80e6178e990d5ae2671fce6cd8f2df57d3f46a56
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-04-14 23:19:35 +0300

    refactor button for CardList

--- src/n2-features/f2-cards/packs/PackList.tsx
+++ src/n2-features/f2-cards/packs/PackList.tsx
@@ -7,20 +7,18 @@ import {Navigate} from 'react-router-dom';
 import {Header} from '../../../n1-main/m1-ui/heder/Header';
 import SuperButton from '../../../n1-main/m1-ui/common/c2-SuperButton/SuperButton';
 import {Sidebar} from '../../../n1-main/m1-ui/Sidebar/Sidebar';
-import {Search} from '../../f1-auth/a7-search/Search';
-import style from '../../../n1-main/m1-ui/styles/PackPage.module.css';
-import {DoubleRange} from '../../f1-auth/a8-selector/doubleRange/DoubleRange';
-import {PacksTableHeader} from './PacksTableHeader';
+import {PackType} from '../../../n1-main/m3-dal/m1-API/packsAPI';
 import {PacksTable} from './packsTable/PacksTable';
 
-export const PackList = React.memo(() => {
+export const PackList = () => {
     const dispatch = useDispatch()
+    const packs = useSelector<AppStoreType, PackType []>(state => state.packs.cardPacks)
     const isLoggedIn = useSelector<AppStoreType, boolean>(state => state.login.isLoggedIn)
     const myPacks = useSelector<AppStoreType, MyPackType>(state => state.packs.myPacks)
 
     useEffect(() => {
         dispatch(fetchPacksTC())
-    }, [dispatch, myPacks])
+    }, [dispatch,myPacks])
 
     const onClickAddNewPackHandler = () => {
         dispatch(addPackTC('!!!!New pack!!!'))
@@ -31,50 +29,13 @@ export const PackList = React.memo(() => {
     }
 
     return (
-        <div>
+        <>
             <Header/>
-            <div className={style.mainContainer}>
-                <div className={style.container_log}>
-                    <div className={style.blockAvatar}>
-                        <div className={style.avatarUrl}>
-                            <h2>Show Cards Packs</h2>
-                            <Sidebar/>
-                        </div>
-                        <div className={style.descriptionForDoubleRangeSlider}>Cards count in a pack</div>
-                        <div className={style.DoubleRangeSliderContainer}>
-                            <DoubleRange/>
-                        </div>
-                    </div>
-                    <div className={style.packsBlock}>
-                        <h1 className={style.titleCardsBlock}> Pack list</h1>
-                        <div className={style.searchAddBlock}>
-                            <Search/>
-                            <SuperButton className={style.btnContainer}
-                                         onClick={onClickAddNewPackHandler}>
-                                Add new Pack
-                            </SuperButton>
-                        </div>
-                        <div className={style.mainTable}>
-                            <PacksTableHeader/>
-                            <PacksTable/>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-        // <>
-        //     <Header/>
-        //     <Sidebar/>
-        //     <h2>Pack List</h2>
-        //     <div style={{width:'300px'}}  >
-        //         <Search/>
-        //         <SuperButton onClick={onClickAddNewPackHandler}>Add new pack</SuperButton>
-        //     </div>
-        //
-        //     <PacksTable/>
-        // </>
+            <Sidebar/>
+            <h2>Pack List</h2>
+            <SuperButton onClick={onClickAddNewPackHandler}>Add new pack</SuperButton>
+            <PacksTable packs={packs}/>
+        </>
     );
-});
+};
 
--- src/n2-features/f2-cards/packs/packsTable/Pack.tsx
+++ src/n2-features/f2-cards/packs/packsTable/Pack.tsx
@@ -6,6 +6,7 @@ import {AppStoreType} from '../../../../n1-main/m2-bll/store';
 import {AppStatusType} from '../../../../n1-main/m2-bll/b1-reducers/appReducer';
 import s from './Pack.module.css'
 import {deletePackTC, updatePackTC} from '../../../../n1-main/m2-bll/b1-reducers/packReducer';
+import {NavLink} from 'react-router-dom';
 
 type PackPropsType = {
     pack: PackType
@@ -29,14 +30,16 @@ export const Pack: React.FC<PackPropsType> = ({pack}) => {
 
     return (
         <div className={s.item}>
-            <div className={s.nameItem}>{pack.name}</div>
+            <NavLink to={`/cards/${pack._id}`}>
+                <div className={s.nameItem}>{pack.name}</div>
+            </NavLink>
             <div className={s.cardsCount}>{pack.cardsCount}</div>
             <div className={s.updated}>{pack.updated.slice(0, 10)}</div>
             <div className={s.buttons}>{myUserId === pack.user_id &&
                 <>
-                    <button className={s.btn}  onClick={onClickDeletePackHandler}>Delete</button>
-                    <button  className={s.btn} onClick={onClickUpdatePackHandler}>Edit</button>
-                    <button className={s.btn} >Learn</button>
+                    <button onClick={onClickDeletePackHandler}>Delete</button>
+                    <button onClick={onClickUpdatePackHandler}>Edit</button>
+                    <button>Learn</button>
                 </>
             }
             </div>
