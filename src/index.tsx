5c9453db9842c14d7d492a1c30bb8249b2f89abe
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-04-15 13:04:42 +0300

    add search

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import {App} from './n1-main/m1-ui/app/App';
 import {Provider} from 'react-redux';
 import {store} from './n1-main/m2-bll/store';
 import {HashRouter} from 'react-router-dom';
-
+//
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
