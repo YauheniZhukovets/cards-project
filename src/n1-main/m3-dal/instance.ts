3b7c3811a87d1cac17afee395c6b883cfc38dbb0
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-04-17 12:49:47 +0300

    fix bags

--- src/n1-main/m3-dal/instance.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const instance = axios.create({
-    baseURL: 'https://neko-back.herokuapp.com/2.0',
-    //baseURL: 'http://localhost:7542/2.0/',
-    withCredentials: true,
-})
--- src/n2-features/f1-auth/a4-forgot/Forgot.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Forgot = () => {
-    return (
-        <div>
-            <span>Forgot</span>
-        </div>
-    );
-};
